6cf40ff36e0626ad99e9dc01c03dae6a55240ab2
Russel Winder <russel@winder.org.uk>
2017-07-18 11:40:27 +0100

    Update the deployment script to not have symbolic links deployed.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,17 +9,21 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
+# NB accuconf_cfp is the primary application and should be copied to the deployment area in archive mode.
+# accuconf_api relies on some files in accuconf_cfp by symbolic link and so cannot use archive mode – the
+# deployment areas are self-contained so cannot have symbolic links.
+
 case $1 in
     cfp.testconference | cfp.conference )
         chmod -R go+rX accuconf*
         destination=conference@dennis.accu.org:/srv/$1.accu.org/public/htdocs/
-        rsync -rav --delete  --exclude=__pycache__/ accuconf_cfp models utils $destination
+        rsync -av --delete  --exclude=__pycache__/ accuconf_cfp models utils $destination
         scp accuconf_cfp.wsgi $destination
         ;;
     api.testconference | api.conference )
         chmod -R go+rX accuconf_api
         destination=conference@dennis.accu.org:/srv/$1.accu.org/public/htdocs/
-        rsync -rav --delete  --exclude=__pycache__/ accuconf_api models utils $destination
+        rsync -rLptgov --delete  --exclude=__pycache__/ accuconf_api models utils $destination
         scp accuconf_api.wsgi $destination
         ;;
     *)
