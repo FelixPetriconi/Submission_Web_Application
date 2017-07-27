fd3c25cc1540cb989ad93589d09dd69a9c0cf9e6
Russel Winder <russel@winder.org.uk>
2017-07-27 17:43:24 +0100

    Avoid leaving the server process in place.

--- tests_cfp__selenium/common.py
+++ tests_cfp__selenium/common.py
@@ -17,19 +17,19 @@ port = '65530'
 base_url = 'http://{}:{}/'.format(host, port)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def server():
-    process = subprocess.Popen('python3 {}'.format(this_directory / 'start_server.py'), shell=True)
+    # NB do not spawn with a shell since then you can't terminate the server.
+    process = subprocess.Popen(('python3', '{}'.format(this_directory / 'start_server.py')))
     time.sleep(2)  # Need a short while for the server to settle, 0.5 works locally but Travis-CI needs longer.
     process.poll()
-    assert process.returncode is None, process.returncode
+    assert process.returncode is None, 'Server start return code {}'.format(process.returncode)
     yield
     process.poll()
-    assert process.returncode is None, process.returncode
-    process.kill()
-    time.sleep(0.5)  # We need to give the OS chance to kill the process.
-    process.poll()
-    assert process.returncode == -9, process.returncode
+    assert process.returncode is None, 'Server used return code P{.'.format(process.returncode)
+    process.terminate()
+    process.wait()
+    assert process.returncode == -15, 'Server terminated return code {}.'.format(process.returncode)
 
 
 @pytest.fixture
