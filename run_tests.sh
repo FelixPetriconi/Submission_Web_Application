0baa2e36475b247346fd308e33e0635234f17c65
Russel Winder <russel@winder.org.uk>
2017-08-17 11:35:05 +0100

    Update directories for coverage reports.

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pytest tests_api --cov accuconf_api
-pytest tests_cfp --cov accuconf_cfp
-pytest tests_cfp__selenium --cov accuconf_cfp
+pytest tests_api --cov accuconf_api  --cov-report html:cov_html_accuconf_api
+pytest tests_cfp --cov accuconf_cfp --cov-report html:cov_html_accuconf_cfp
+pytest tests_cfp__selenium --cov accuconf_cfp  --cov-report html:cov_html_accuconf_cfp__selenium
 npm test
