ed0cabd41086c1c8359c0487ba26380af9fde7a8
Russel Winder <russel@winder.org.uk>
2017-10-05 14:52:45 +0100

    Print the browser log.

--- tests_cfp__selenium/fixtures.py
+++ tests_cfp__selenium/fixtures.py
@@ -48,6 +48,6 @@ def driver():
     options.add_argument('headless')
     wd = Chrome(chrome_options=options, desired_capabilities=capabilities)
     yield wd
-    # print('Browser', wd.get_log('browser'))
+    print('Browser', wd.get_log('browser'))
     # print('Driver', wd.get_log('driver'))
     wd.quit()
