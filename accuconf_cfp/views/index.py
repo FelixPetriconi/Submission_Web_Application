162b9c89bd83c7beb012fc86bd65faf143e07413
Russel Winder <russel@winder.org.uk>
2017-10-20 15:35:53 +0100

    A quick hack to get a connection from the CFP application front page to the main website.

--- accuconf_cfp/views/index.py
+++ accuconf_cfp/views/index.py
@@ -22,6 +22,24 @@ This should not take long, so please come back soon.
         return render_template('general.html', page=md(page, {'data': Markup('''
 The ACCU {} Call for Proposals is open for business.
 </p>
+<i>
+<p>
+This Web application is just for submitting proposals, the
+<a href="https://conference.accu.org">main website</a>
+ is where to go to get information about the conference. 
+</p>
+<p>
+There are two blog posts that might be particularly interesting for submitters:
+</p>
+<ul>
+<li>
+<a href="https://conference.accu.org/posts/2017/201710150748_proposalsforaccu2018.html">Proposals for ACCU 2018</a>
+</li>
+<li>
+<a href="https://conference.accu.org/posts/2017/201710150836_whatsaccuabout.html">What's ACCU About</a> 
+</li>
+</ul>
+</i>
 <p>
 Using the menu items on the left, you can register, if you are not already registered,
 or login if you are already registered – you have to have registered in order to login.
