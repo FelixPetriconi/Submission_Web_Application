41f71897be630ab79c1981f9f474a7652d9829d8
Russel Winder <russel@winder.org.uk>
2017-12-29 12:17:09 +0000

    Add the acceptance emailouts.

---
+++ emailouts/accept_preconference_workshops/query.py
@@ -0,0 +1,14 @@
+from models.proposal import Proposal
+from models.proposal_types import ProposalState, SessionType
+
+
+def query():
+    proposals = Proposal.query.filter_by(session_type=SessionType.fulldayworkshop)
+    proposals = [p for p in proposals if p.status == ProposalState.accepted]
+    assert len(proposals) == 4
+    return tuple((p, p.proposer) for p in proposals)
+
+
+def edit_template(template_path, proposal, person):
+    with open(template_path) as template_file:
+        return template_file.read().strip().format(person.name, proposal.title)
---
+++ emailouts/send_out_session_acceptances/query.py
@@ -0,0 +1,26 @@
+from models.proposal import Proposal
+from models.proposal_types import ProposalState, SessionType
+
+
+def query():
+    proposals = Proposal.query.filter_by(status=ProposalState.accepted)
+    proposals = [p for p in proposals if p.session_type != SessionType.fulldayworkshop]
+    all(p.session_type in (SessionType.session, SessionType.miniworkshop, SessionType.workshop, SessionType.quickie) for p in proposals)
+    return tuple((p, p.proposer) for p in proposals)
+
+
+def edit_template(template_path, proposal, person):
+    with open(template_path) as template_file:
+        return template_file.read().strip().format(person.name, proposal.session_type.value, proposal.title,
+"""
+As this session is a quickie, the conference is not able to provide a
+'presenter package' for putting on this session, the presenter will have to
+register for the conference via some other route.
+""" if proposal.session_type == SessionType.quickie else """
+As this is a full session, the presenter, or lead presenter for multi-
+presenter sessions, will be offered a 'presenter deal' of free conference
+ticket, contribution to travel expenses, and two nights accommodation at
+the conference hotel.
+
+See https://conference.accu.org/lead_presenter_deals.html
+""")
