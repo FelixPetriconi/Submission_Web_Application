1fc164b42f9a437ed545864e4dab56537fe2ae5d
Russel Winder <russel@winder.org.uk>
2018-01-27 18:05:35 +0000

    Add the sub-edit emailout.

---
+++ emailouts/subedit_accepted/query.py
@@ -0,0 +1,18 @@
+from models.proposal import Proposal
+from models.proposal_types import ProposalState
+
+
+def query():
+    proposals = Proposal.query.filter_by(status=ProposalState.acknowledged)
+    return tuple((p, p.proposer) for p in proposals)
+
+
+def edit_template(template_path, proposal, person):
+    with open(template_path) as template_file:
+        return template_file.read().strip().format(
+            person.name,
+            proposal.session_type.value,
+            proposal.title,
+            proposal.summary,
+            '\n\n'.join('{}\n\n{}'.format(p.name, p.bio) for p in proposal.presenters)
+        )
