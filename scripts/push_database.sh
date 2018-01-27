a2af199ceb8c4c99babcf285f81be24a603bab2c
Russel Winder <russel@winder.org.uk>
2018-01-27 17:24:30 +0000

    Rewrite the database push script a bit.

--- scripts/push_database.sh
+++ scripts/push_database.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+push_it() {
+    source_file=$1
+    destination_directory=conference@dennis.accu.org:/srv/$3.$2.accu.org/public/htdocs/
+    echo -n "
+About to push:  $source_file
+to:  $destination_directory
+
+is this OK? "
+    read answer
+    echo ""
+    if [ x$answer = xyes ]; then
+        scp $source_file $destination_directory
+    else
+        echo "No action taken."
+    fi
+}
+
 destination=$1
 case $destination in
     test)
-        destination_directory=cfp.testconference.accu.org
+        destination_directory_root=testconference
         ;;
     live)
-        destination_directory=cfp.conference.accu.org
+        destination_directory_root=conference
         ;;
     *)
         echo "'$destination' is not a valid source."
@@ -14,16 +31,5 @@ case $destination in
         ;;
 esac
 source_file=$(pwd)/accuconf.db
-destination_directory=conference@dennis.accu.org:/srv/$destination_directory/public/htdocs/
-echo -n "
-About to push:  $source_file
-to:  $destination_directory
-
-is this OK? "
-read answer
-echo ""
-if [ x$answer = xyes ]; then
-    scp $source_file $destination_directory
-else
-    echo "No action taken."
-fi
+push_it $source_file $destination_directory_root cfp
+push_it $source_file $destination_directory_root api
